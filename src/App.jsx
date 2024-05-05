c415d88de0661efb00f03a419e35f0d2d67d53ec
Vidit Modi <96867068+Vidit2207@users.noreply.github.com>
2024-05-05 12:29:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./UI/styles/styles.scss";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>A great project in the making</h1>
+    </Fragment>
+  );
+}
+
+export default App;
